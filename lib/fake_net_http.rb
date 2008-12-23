e986b721c744afb1fa389cd593629a3dd525616e
Chris Kampmeier <chris@kampers.net>
2008-12-23 02:53:46 -0500

    Code cleanup and minor refactoring in lib/fake_net_http.rb

--- lib/fake_net_http.rb
+++ lib/fake_net_http.rb
@@ -1,16 +1,16 @@
 # FakeWeb - Ruby Helper for Faking Web Requests
 # Copyright 2006 Blaine Cook <romeda@gmail.com>.
-# 
+#
 # FakeWeb is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # FakeWeb is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with FakeWeb; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -19,54 +19,55 @@ require 'net/http'
 require 'net/https'
 require 'stringio'
 
-module Net #:nodoc:
+module Net  #:nodoc:
 
-  class BufferedIO #:nodoc:
-    def initialize( io, debug_output = nil )
+  class BufferedIO  #:nodoc:
+    def initialize(io, debug_output = nil)
       @read_timeout = 60
       @rbuf = ''
       @debug_output = debug_output
+
       @io = case io
-      when Socket, OpenSSL::SSL::SSLSocket, IO: io
+      when Socket, OpenSSL::SSL::SSLSocket, IO
+        io
       when String
-        File.exists?(io) ?  File.open(io, "r") : StringIO.new(io)
+        File.exists?(io) ? File.open(io, "r") : StringIO.new(io)
       end
       raise "Unable to create local socket" unless @io
     end
-
   end
-  
-  class HTTP #:nodoc:
 
-    def HTTP.socket_type #:nodoc:
+
+  class HTTP  #:nodoc:
+    def self.socket_type
       FakeWeb::SocketDelegator
     end
 
     alias :original_net_http_request :request
     alias :original_net_http_connect :connect
-    
-    def request(req, body = nil, &block)
-      prot = use_ssl ? "https" : "http"
 
-      path = req.path
-      path = URI.parse(req.path).request_uri if req.path =~ /^http/
+    def request(request, body = nil, &block)
+      protocol = use_ssl ? "https" : "http"
 
-      uri = "#{prot}://#{self.address}:#{self.port}#{path}"
+      path = request.path
+      path = URI.parse(request.path).request_uri if request.path =~ /^http/
+
+      uri = "#{protocol}://#{self.address}:#{self.port}#{path}"
 
       if FakeWeb.registered_uri?(uri)
         @socket = Net::HTTP.socket_type.new
-        return FakeWeb.response_for(uri, &block)
+        FakeWeb.response_for(uri, &block)
+      elsif FakeWeb.allow_net_connect?
+        original_net_http_connect
+        original_net_http_request(request, body, &block)
       else
-        if FakeWeb.allow_net_connect?
-          original_net_http_connect
-          return original_net_http_request(req, body, &block)
-        else
-          raise FakeWeb::NetConnectNotAllowedError, "Real HTTP connections are disabled. Unregistered URI: #{uri}"
-        end
+        raise FakeWeb::NetConnectNotAllowedError,
+              "Real HTTP connections are disabled. Unregistered URI: #{uri}"
       end
     end
 
     def connect
     end
   end
+
 end

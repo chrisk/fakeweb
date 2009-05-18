ca7c75ecf7101b91b6fc2ae76af58e7f561709ee
Chris Kampmeier <chris@kampers.net>
2009-05-18 08:45:40 -0700

    Add a proper test for 96d551109b45a5d872d337a73c4ae4ddc370c865 by dynamically undefining OpenURI

---
+++ test/test_missing_open_uri.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), "test_helper")
+
+class TestMissingOpenURI < Test::Unit::TestCase
+
+  def setup
+    FakeWeb.clean_registry
+    @saved_open_uri = OpenURI
+    Object.send(:remove_const, :OpenURI)
+  end
+
+  def teardown
+    Object.const_set(:OpenURI, @saved_open_uri)
+  end
+
+
+  def test_register_using_exception_without_open_uri
+    # regression test for Responder needing OpenURI::HTTPError to be defined
+    FakeWeb.register_uri(:get, "http://example.com/", :exception => StandardError)
+    assert_raises(StandardError) do
+      Net::HTTP.start("example.com") { |http| http.get("/") }
+    end
+  end
+
+end

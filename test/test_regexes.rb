6eb7d0865dd4522a4c6da2fbfcef3a897016b96d
Chris Kampmeier <chris@kampers.net>
2009-05-24 17:09:46 -0700

    Clean up test coverage for regexes

--- test/test_regexes.rb
+++ test/test_regexes.rb
@@ -13,49 +13,50 @@ class TestRegexes < Test::Unit::TestCase
 
   def fake_pattern_match
     @fake_pattern_match ||= [
-      {:pattern => %r|http://www.yahoo.com|, :responders => [FakeWeb::Responder.new(:get, "http://www.yahoo.com", {:response => 'Welcome to Yahoo!'}, 1)], :method => :get},
-      {:pattern => %r|https://www.yahoo.com|, :responders => [FakeWeb::Responder.new(:get, "https://www.yahoo.com", {:response => 'Welcome to secure Yahoo!'}, 1)], :method => :get}
+      {:pattern => %r|http://www.example.com|, :responders => [FakeWeb::Responder.new(:get, "http://www.example.com", {:response => "Welcome!"}, 1)], :method => :get},
+      {:pattern => %r|https://www.example.com|, :responders => [FakeWeb::Responder.new(:get, "https://www.example.com", {:response => "Secure welcome!"}, 1)], :method => :get}
     ]
   end
 
   def test_pattern_map_matches
     FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert_equal [fake_pattern_match.first], FakeWeb::Registry.instance.send(:pattern_map_matches, :get, "http://www.yahoo.com")
+    assert_equal [fake_pattern_match.first], FakeWeb::Registry.instance.send(:pattern_map_matches, :get, "http://www.example.com")
   end
 
   def test_pattern_map_match
     FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert_equal fake_pattern_match.first, FakeWeb::Registry.instance.send(:pattern_map_match, :get, "http://www.yahoo.com")
+    assert_equal fake_pattern_match.first, FakeWeb::Registry.instance.send(:pattern_map_match, :get, "http://www.example.com")
   end
 
   def test_register_uri_pattern
-    FakeWeb.register_uri(%r|http://mock/test_example/\d+|, :string => "example")
-    assert FakeWeb.registered_uri?('http://mock/test_example/25')
+    FakeWeb.register_uri(%r|http://example.com/test_example/\d+|, :string => "example")
+    assert FakeWeb.registered_uri?("http://example.com/test_example/25")
+    assert !FakeWeb.registered_uri?("http://example.com/test_example/abc")
   end
 
   def test_pattern_map_can_find_matches
     FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.yahoo.com")
+    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.example.com")
   end
 
   def test_pattern_map_can_find_matches_with_port
     FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.yahoo.com:80")
+    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.example.com:80")
   end
 
   def test_pattern_map_can_find_matches_with_port_and_trailing_slash
     FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.yahoo.com:80/")
+    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.example.com:80/")
   end
 
   def test_pattern_map_can_find_matches_with_https_port
     FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "https://www.yahoo.com:443")
+    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "https://www.example.com:443")
   end
 
   def test_pattern_map_can_find_matches_with_https_port_and_trailing_slash
     FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "https://www.yahoo.com:443/")
+    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "https://www.example.com:443/")
   end
 
   def test_registering_with_overlapping_regexes_uses_first_registered
@@ -66,9 +67,9 @@ class TestRegexes < Test::Unit::TestCase
   end
 
   def test_registry_does_not_find_using_mismatched_protocols_or_ports_when_registered_with_both
-    FakeWeb.register_uri(:get, %r|http://www.yahoo.com:80|, :response => "Welcome to Yahoo!")
-    assert !FakeWeb.registered_uri?(:get, "https://www.yahoo.com:80")
-    assert !FakeWeb.registered_uri?(:get, "http://www.yahoo.com:443")
+    FakeWeb.register_uri(:get, %r|http://www.example.com:80|, :string => "example")
+    assert !FakeWeb.registered_uri?(:get, "https://www.example.com:80")
+    assert !FakeWeb.registered_uri?(:get, "http://www.example.com:443")
   end
 
   def test_registry_only_finds_using_default_port_when_registered_without_if_protocol_matches
@@ -83,16 +84,16 @@ class TestRegexes < Test::Unit::TestCase
   end
 
   def test_registry_matches_using_mismatched_port_when_registered_without
-    FakeWeb.register_uri(:get, %r|http://www.yahoo.com|, :response => "Welcome to Yahoo!")
-    assert FakeWeb.registered_uri?(:get, "http://www.yahoo.com:80")
-    assert FakeWeb.registered_uri?(:get, "http://www.yahoo.com:443")
-    assert FakeWeb.registered_uri?(:get, "http://www.yahoo.com:12345")
-    assert !FakeWeb.registered_uri?(:get, "https://www.yahoo.com:443")
-    assert !FakeWeb.registered_uri?(:get, "https://www.yahoo.com")
+    FakeWeb.register_uri(:get, %r|http://www.example.com|, :string => "example")
+    assert FakeWeb.registered_uri?(:get, "http://www.example.com:80")
+    assert FakeWeb.registered_uri?(:get, "http://www.example.com:443")
+    assert FakeWeb.registered_uri?(:get, "http://www.example.com:12345")
+    assert !FakeWeb.registered_uri?(:get, "https://www.example.com:443")
+    assert !FakeWeb.registered_uri?(:get, "https://www.example.com")
   end
 
   def test_registry_matches_using_any_protocol_and_port_when_registered_without_protocol_or_port
-    FakeWeb.register_uri(:get, %r|www.example.com|, :response => "example")
+    FakeWeb.register_uri(:get, %r|www.example.com|, :string => "example")
     assert FakeWeb.registered_uri?(:get, "http://www.example.com")
     assert FakeWeb.registered_uri?(:get, "http://www.example.com:80")
     assert FakeWeb.registered_uri?(:get, "http://www.example.com:443")

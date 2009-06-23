206de4bf4310b5e78c3054604afb1855cef9adfb
Chris Kampmeier <chris@kampers.net>
2009-06-23 01:21:53 -0700

    Remove old regex tests that check the internals, now that there are higher-level tests that check the same things using the public API

--- test/test_regexes.rb
+++ test/test_regexes.rb
@@ -11,52 +11,10 @@ class TestRegexes < Test::Unit::TestCase
     FakeWeb.allow_net_connect = @old_allow_net_conncet
   end
 
-  def fake_pattern_match
-    @fake_pattern_match ||= [
-      {:pattern => %r|http://www.example.com|, :responders => [FakeWeb::Responder.new(:get, "http://www.example.com", {:response => "Welcome!"}, 1)], :method => :get},
-      {:pattern => %r|https://www.example.com|, :responders => [FakeWeb::Responder.new(:get, "https://www.example.com", {:response => "Secure welcome!"}, 1)], :method => :get}
-    ]
-  end
-
-  def test_pattern_map_matches
-    FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert_equal [fake_pattern_match.first], FakeWeb::Registry.instance.send(:pattern_map_matches, :get, "http://www.example.com")
-  end
-
-  def test_pattern_map_match
-    FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert_equal fake_pattern_match.first, FakeWeb::Registry.instance.send(:pattern_map_match, :get, "http://www.example.com")
-  end
-
-  def test_register_uri_pattern
+  def test_registered_uri_with_pattern
     FakeWeb.register_uri(:get, %r|http://example.com/test_example/\d+|, :body => "example")
     assert FakeWeb.registered_uri?(:get, "http://example.com/test_example/25")
     assert !FakeWeb.registered_uri?(:get, "http://example.com/test_example/abc")
-  end
-
-  def test_pattern_map_can_find_matches
-    FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.example.com")
-  end
-
-  def test_pattern_map_can_find_matches_with_port
-    FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.example.com:80")
-  end
-
-  def test_pattern_map_can_find_matches_with_port_and_trailing_slash
-    FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "http://www.example.com:80/")
-  end
-
-  def test_pattern_map_can_find_matches_with_https_port
-    FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "https://www.example.com:443")
-  end
-
-  def test_pattern_map_can_find_matches_with_https_port_and_trailing_slash
-    FakeWeb::Registry.any_instance.stubs(:pattern_map).returns(fake_pattern_match)
-    assert FakeWeb::Registry.instance.send(:pattern_map_matches?, :get, "https://www.example.com:443/")
   end
 
   def test_response_for_with_matching_registered_uri

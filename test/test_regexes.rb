92a63cdcf652a386e8a3accf3573451f9b815f86
Chris Kampmeier <chris@kampers.net>
2009-06-26 04:05:07 -0700

    Fill in some missing regexes test coverage

--- test/test_regexes.rb
+++ test/test_regexes.rb
@@ -22,6 +22,11 @@ class TestRegexes < Test::Unit::TestCase
     assert_equal "Welcome to Google!", FakeWeb.response_for(:get, "http://www.google.com").body
   end
 
+  def test_response_for_with_matching_registered_uri_and_get_method_matching_to_any_method
+    FakeWeb.register_uri(:any, %r|http://www.example.com|, :body => "example")
+    assert_equal "example", FakeWeb.response_for(:get, "http://www.example.com").body
+  end
+
   def test_registered_uri_with_authentication_and_pattern
     FakeWeb.register_uri(:get, %r|http://user:pass@mock/example\.\w+|i, :body => "example")
     assert FakeWeb.registered_uri?(:get, 'http://user:pass@mock/example.txt')

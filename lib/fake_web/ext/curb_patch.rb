483c12bc35ef5f8ea7f5a3c0ccb4e24c3df14f0b
Chris Kampmeier <chris@kampers.net>
2009-11-25 13:54:14 -0800

    Spiking some more to handle Curl::Easy instances properly
    Trying to monkey-patch C is scary/awesome.

--- lib/fake_web/ext/curb_patch.rb
+++ lib/fake_web/ext/curb_patch.rb
@@ -1,21 +1,50 @@
 if defined?(Curl::Easy)
+
+  # TODO: I'm sure there's a better way to do this than a global
+  $reusable_instance = Curl::Easy.new("")
+
   module Curl
     class Easy
 
       class << self
-        def perform_with_fakeweb(uri)
-          if FakeWeb.registered_uri?(:get, uri)
-            FakeWeb::Registry.instance.curl_response_for(:get, uri)
-          elsif FakeWeb.allow_net_connect?
-            perform_without_fakeweb(uri)
-          else
-            raise FakeWeb::NetConnectNotAllowedError,
-                  "Real HTTP connections are disabled. Unregistered request: GET #{uri}"
-          end
+        def new_with_fakeweb(*args, &block)
+          obj = $reusable_instance.dup
+          obj.send :initialize, *args, &block
+          obj
+        end
+        alias_method :new_without_fakeweb, :new
+        alias_method :new, :new_with_fakeweb
+
+        def perform_with_fakeweb(url)
+          curl = new(url)
+          curl.perform
+          curl
         end
         alias_method :perform_without_fakeweb, :perform
         alias_method :perform, :perform_with_fakeweb
       end
+
+      attr_accessor :body_str
+
+      def initialize(url = nil, &block)
+        self.url = url
+        yield if block_given?
+      end
+
+      def perform_with_fakeweb
+        if FakeWeb.registered_uri?(:get, url)
+          r = FakeWeb::Registry.instance.curl_response_for(:get, url)
+          self.body_str = r.body_str
+          true
+        elsif FakeWeb.allow_net_connect?
+          perform_without_fakeweb
+        else
+          raise FakeWeb::NetConnectNotAllowedError,
+                "Real HTTP connections are disabled. Unregistered request: GET #{url}"
+        end
+      end
+      alias_method :perform_without_fakeweb, :perform
+      alias_method :perform, :perform_with_fakeweb
 
     end
   end
--- test/test_curb.rb
+++ test/test_curb.rb
@@ -2,22 +2,37 @@ require 'test_helper'
 
 class TestCurb < Test::Unit::TestCase
 
-  def test_curl_easy_perform
+  def test_curl_easy_class_perform
     FakeWeb.register_uri(:get, "http://example.com", :body => "example")
     response = Curl::Easy.perform("http://example.com")
     assert_equal "example", response.body_str
   end
 
-  def test_curl_easy_perform_2
+  def test_curl_easy_class_perform_with_different_body
     FakeWeb.register_uri(:get, "http://example.com", :body => "example2")
     response = Curl::Easy.perform("http://example.com")
     assert_equal "example2", response.body_str
   end
 
-  def test_easy_perform_with_unregistered_uri_raises
+  def test_curl_easy_instance_perform
+    FakeWeb.register_uri(:get, "http://example.com", :body => "example3")
+    curl = Curl::Easy.new("http://example.com")
+    assert_equal true, curl.perform
+    assert_equal "example3", curl.body_str
+  end
+
+  def test_easy_class_perform_with_unregistered_uri_raises
     assert_raise FakeWeb::NetConnectNotAllowedError do
       Curl::Easy.perform("http://example.com")
     end
   end
+
+  def test_easy_instance_perform_with_unregistered_uri_raises
+    curl = Curl::Easy.new("http://example.com")
+    assert_raise FakeWeb::NetConnectNotAllowedError do
+      curl.perform
+    end
+  end
+
 
 end

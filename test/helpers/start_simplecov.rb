6f712bf825ab2e6cb6a756dbf2456ef61261acb3
Chris Kampmeier <chris@kampers.net>
2013-11-23 19:34:44 -0800

    Several extract-method refactors in code-coverage test helper

--- test/helpers/start_simplecov.rb
+++ test/helpers/start_simplecov.rb
@@ -1,7 +1,7 @@
 module FakeWebTestHelper
   module CodeCoverage
     def start_simplecov
-      return unless RUBY_VERSION >= "1.9.0"
+      return unless coverage_supported_by_this_ruby?
       return if !ENV['AUTOTEST'].nil?
 
       require 'simplecov'
@@ -11,24 +11,34 @@ module FakeWebTestHelper
         add_filter "/test/"
 
         minimum_coverage 100 if running_all_tests?
+        command_name SIMPLECOV_COMMAND_NAME if child_test_process?
+        formatter simplecov_formatter_class
+      end
+    end
 
-        if defined?(SIMPLECOV_COMMAND_NAME)
-          command_name SIMPLECOV_COMMAND_NAME
-        end
-
-        if !ENV["COVERAGE_REPORT"].nil?
-          formatter SimpleCov::Formatter::MultiFormatter[
-            SimpleCov::Formatter::HTMLFormatter,
-            SimpleCov::Formatter::Console
-          ]
-        else
-          formatter SimpleCov::Formatter::Console
-        end
+    def simplecov_formatter_class
+      include SimpleCov::Formatter
+      if html_report_requested?
+        MultiFormatter[HTMLFormatter, Console]
+      else
+        Console
       end
     end
 
     def running_all_tests?
       ENV['TEST'].nil?
+    end
+
+    def child_test_process?
+      defined?(SIMPLECOV_COMMAND_NAME)
+    end
+
+    def html_report_requested?
+      !ENV["COVERAGE_REPORT"].nil?
+    end
+
+    def coverage_supported_by_this_ruby?
+      RUBY_VERSION >= "1.9.0"
     end
 
     extend self
